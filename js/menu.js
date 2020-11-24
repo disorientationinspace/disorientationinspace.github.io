d157d3d992dc47787f83738ef6fd09dae54771fe
disorientationinspace <disorientationinspace@gmail.com>
2020-11-24 23:11:03 +0300

    NL Label v1.0.0

--- js/menu.js
+++
@@ -1,1 +0,0 @@
-var header=document.querySelector(".main-header"),open=document.querySelector(".main-header__open"),close=document.querySelector(".main-header__close");header.classList.remove("main-header--no-js"),open.addEventListener("click",function(){header.classList.add("main-header--active")}),close.addEventListener("click",function(){header.classList.remove("main-header--active")});
